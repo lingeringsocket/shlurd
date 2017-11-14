33d0a0a73d33ee2328723020aca984f34d53efcc
John Sichi <jsichi@gmail.com>
2017-11-13 19:26:23 -0800

    Add Apache license

--- openhab/com.lingeringsocket.shlurd.openhab/src/main/java/com/lingeringsocket/shlurd/openhab/ShlurdHumanLanguageInterpreter.java
+++ openhab/com.lingeringsocket.shlurd.openhab/src/main/java/com/lingeringsocket/shlurd/openhab/ShlurdHumanLanguageInterpreter.java
@@ -1,9 +1,18 @@
 /**
- * Copyright (c) 2014-2017 by the respective copyright holders.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+// shlurd:  a limited understanding of small worlds
+// Copyright 2017-2017 John V. Sichi
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
  */
 package com.lingeringsocket.shlurd.openhab;
 
@@ -52,8 +61,7 @@ import spire.math.Trilean$;
 /**
  * A human language command interpretation service based on SHLURD.
  *
- * @author John Sichi - Initial contribution
- *
+ * @author John Sichi
  */
 public class ShlurdHumanLanguageInterpreter extends AbstractRuleBasedInterpreter
 {
