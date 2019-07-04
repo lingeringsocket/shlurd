55513accc3e0614c94b73727392ccb7800d7c134
John Sichi <jsichi@gmail.com>
2019-07-03 23:19:32 -0700

    Oops

---
+++ corenlp/src/test/scala/com/lingeringsocket/shlurd/corenlp/CorenlpSpec.scala
@@ -0,0 +1,41 @@
+// shlurd:  a limited understanding of small worlds
+// Copyright 2017-2018 John V. Sichi
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package com.lingeringsocket.shlurd.corenlp
+
+import com.lingeringsocket.shlurd.parser._
+import com.lingeringsocket.shlurd.ilang._
+import com.lingeringsocket.shlurd.mind._
+
+// we selectively import suites known to work with CoreNLP
+
+class CorenlpSentencePrinterSpec extends SilSentencePrinterSpec
+{
+}
+
+class CorenlpSentenceTranslatorSpec extends SilSentenceTranslatorSpec
+{
+}
+
+class CorenlpParserSpec extends SprParserSpec
+{
+}
+
+class CorenlpEnglishVerbSpec extends SprEnglishVerbSpec
+{
+}
+
+class CorenlpResponderSpec extends SmcResponderSpec
+{
+}
